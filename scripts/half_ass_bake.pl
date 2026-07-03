acf254540c7a0e46d3e5fc960a25b34550e150e1
FedericoCMartini <martini.federico@proton.me>
2026-07-03 23:06:58 +0200

    half assing the jump table address baking script

---
+++ scripts/half_ass_bake.pl
@@ -0,0 +1,22 @@
+#!/bin/perl
+
+my $table = `cat ./dump`; #you'll need to paste the addresses here from ripes disassemble
+
+while(<>)
+{
+    my $line = $_;
+    if ($line =~ /.*_jump_table:/) {
+        
+        my @symbols = ( $line =~ /\w*_case_\w*/g);
+        foreach (@symbols)
+        {
+            my $name = $_;
+            my ($symbol_hex) = $table =~ /([\da-f]*)\s*<$name>:/;
+
+            $line =~ s/$name/0x$symbol_hex/;
+        }
+    }
+    unless ( $line =~ /^\s*#\s*$/) { #skips line where it's just a '#'
+        print $line;
+    }
+}
